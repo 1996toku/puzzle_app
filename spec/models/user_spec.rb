a11b0fcd1645ce62d9b3b415e484e45a4d79c1e0
1996toku <19960327toku@gmail.com>
2020-09-29 18:47:00 +0900

    rubocopの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,23 +23,23 @@ RSpec.describe User, type: :model do
       end
 
       context 'ユーザー登録がうまくいかないとき' do
-        it "nameが空では登録できないこと" do
+        it 'nameが空では登録できないこと' do
           @user.name = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Name can't be blank")
         end
-        it "passwordが空では登録できないこと" do
+        it 'passwordが空では登録できないこと' do
           @user.password = nil
           @user.valid?
           expect(@user.errors.full_messages).to include("Password can't be blank")
         end
-        it "passwordが6文字以上出なければ登録できないこと" do
+        it 'passwordが6文字以上出なければ登録できないこと' do
           @user.password = '12345'
           @user.password_confirmation = '12345'
           @user.valid?
-          expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+          expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
         end
-        it "passwordとpassword_confirmationが同じでなければ登録できないこと" do
+        it 'passwordとpassword_confirmationが同じでなければ登録できないこと' do
           @user.password = '123456'
           @user.password_confirmation = '223456'
           @user.valid?
@@ -47,6 +47,5 @@ RSpec.describe User, type: :model do
         end
       end
     end
-
   end
 end
